225cecdfc2024fd5dcc08582e9b259d453c6462d
ScholarSphinx <136891555+ScholarSphinx@users.noreply.github.com>
2024-05-01 17:18:22 +0200

    Add files via upload

---
+++ WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿
+namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(68, 260);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Write To Textfile";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(253, 260);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(108, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Read one line";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(437, 260);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(132, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Read multiple lines";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(68, 323);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(103, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Append To Textfile";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(68, 384);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(103, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Advanced Writing";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "Calvin",
+            "John",
+            "Asogaran",
+            "Pillay",
+            "Mariemuthu",
+            "Sebastian",
+            "Elijah"});
+            this.listBox1.Location = new System.Drawing.Point(428, 61);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(205, 134);
+            this.listBox1.TabIndex = 6;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(253, 323);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(132, 23);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Read one line another way";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(68, 61);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(256, 134);
+            this.listBox2.TabIndex = 8;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(437, 323);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(152, 23);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "Read multiple lines another way";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(662, 220);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ WindowsFormsApp4/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+//for texfiles we are using System.IO
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //a textfile is a permanent data storage
+            //There are two main functions of a textfile
+            //Read from a textfile --> Input - datasource that we are reading.
+            //Write to a textfile --> Output - giving the datasource information/data
+
+            //string, double, integer, decimal, bool,
+            //Read - StreamReader
+            //Write - StreamWriter
+
+            //string name;
+            //StreamReader datasource;
+            //StreamWriter datasource
+
+            //Two types of writing
+            //Rewriting a textfile - assume the file doesn't exist, and you replce any exsiting data
+            //eg. Notice --> Noted
+            //Appending to a textfile - assumes the file exists, and you add on to the existing content
+            //eg. Notice --> NoticeNoted
+
+            StreamWriter output;
+            output = File.CreateText("test.txt");
+            //.txt - this is the file extension for a textfile --> DO NOT LEAVE THIS OUT
+            //Rewriting a file
+
+
+            //output.Write("1");
+            //output.Write("2");
+            //output.Write("3");
+
+            //123
+
+            //output.WriteLine("1");
+            //output.WriteLine("2");
+            //output.WriteLine("3");
+
+            //1
+            //2
+            //3
+
+            output.Write("1");
+            output.Write("2");
+            output.Write("3");
+
+            //close the textfile
+            //if you do not close it and run a method or press a button that uses the same textfile it will break!
+            output.Close();
+            MessageBox.Show("Written to textfile");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            StreamWriter output;
+            //[variable type] [variable name];\
+            output = File.AppendText("test.txt");
+            //Appending a file
+
+            
+
+            output.Write("1");
+            output.Write("2");
+            output.Write("3");
+
+            output.WriteLine("1");
+            output.WriteLine("2");
+            output.WriteLine("3");
+
+            output.Close();
+            MessageBox.Show("Appended to textfile");
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            
+            StreamWriter output = File.AppendText("test.txt");
+            //[variable type] [variable name] = File.AppendText("[file name.txt]");
+            //[variable type] [variable name] = File.CreateText("[file name.txt]");
+            for (int i = 0; i< listBox1.Items.Count ; i++)
+            //indexes in listboxes start from 0;
+            //we do not need the -1, if we add the minus one, then the final value will be less than the final index. This means that it misses one of the values in the listbox
+            //lets say you have 1 item, the item is the first one, but its index is 0
+            //lets say you have a 2nd item, its 2 items, but the second items index is 1
+            {
+                output.WriteLine(listBox1.Items[i]);
+            }
+            output.Close();
+            MessageBox.Show("Advanced Writing done");
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Getting data from the textfile
+            //Use StreamReader
+            //[variable type] [variable name];
+            StreamReader input;
+            input = File.OpenText("test2.txt");
+
+            //store line data in a variable
+
+            string sLine;
+            sLine = input.ReadLine();
+            MessageBox.Show(sLine);
+
+            //Please please please close the textfile
+            input.Close();
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            StreamReader input;
+            input = File.OpenText("test2.txt");
+            MessageBox.Show(input.ReadLine());
+            input.Close();
+
+            //Difference is that I'm not using the variable sLine
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            StreamReader input;
+            input = File.OpenText("test.txt");
+
+            //We do not know how many lines the textfile has
+
+            //input.EndOfStream
+            //EndOfStream checks for the end of the textfile
+
+            while (input.EndOfStream == false)
+                //while the condition () is true
+            {
+                string sLine;
+                sLine = input.ReadLine();
+                //Reads the first line that has not been read yet.
+                listBox2.Items.Add(sLine);
+            }
+            MessageBox.Show("Job is done");
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            StreamReader input = File.OpenText("test.txt");
+            listBox2.Items.Clear();
+            while (input.EndOfStream == false)
+            {
+                listBox2.Items.Add(input.ReadLine());
+            }
+            MessageBox.Show("Next job is done");
+        }
+    }
+}
